2b36181938f331ddfead459d05c8f4f5e840c372
1155108884 <1155108884@link.cuhk.edu.hk>
2019-04-18 18:46:14 +0800

    update code.c

--- code.c
+++ code.c
@@ -5,7 +5,8 @@ int main()
 {
     printf("Hello\n");
     printf("CSCI3250 team-h\n");
-    for(int i=0;i<3250;i++)
+    int i;
+    for(i=0;i<3250;i++)
     printf("I hate CSCI%d\n",i );
     printf("I love CSCI3250\n");
     return 0;
